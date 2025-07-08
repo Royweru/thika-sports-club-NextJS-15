68aaa2f79d14d6c37fc71b1def8b54e5d9931f4d
Royweru <royezi17@gmail.com>
2025-07-08 06:40:52 +0300

    Added use client to the resource center page

--- app/resource-center/page.tsx
+++ app/resource-center/page.tsx
@@ -1,3 +1,4 @@
+ 'use client'
 import React, { useState } from 'react';
 import { FileText, Download, Eye, X, Calendar, TrendingUp, DollarSign, Settings, ChevronRight, Search} from 'lucide-react';
 interface selectedResourceType{
